c7e6d76edfa6c4e9f83a2fe2144c247a410251c3
anikeydev <dev.anikey.va@mail.ru>
2022-11-28 21:53:35 +0300

    realize cli --format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,8 +5,13 @@ import { Command } from 'commander'
 const program = new Command()
 
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .version('0.0.1')
+
+program
+    .option('-f, --format <type>', 'output format')
 
 program.parse()
